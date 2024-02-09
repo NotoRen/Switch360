a51c245f03dfafe729ba05c6f45411f1c71be446
JoJourdan <d.jourdan.2256@vallauri.edu>
2024-02-09 13:12:56 +0100

    Creazione navbar e header pulsanti

--- Switch360/src/app/main/main.component.ts
+++ Switch360/src/app/main/main.component.ts
@@ -11,4 +11,25 @@ export class MainComponent {
   visModifica:boolean=false;
   visVisualizza:boolean=false;
 
+  onNavigate(feature:string){
+    switch(feature){
+      case "inserisci":
+        this.visInserisci=true;
+        this.visModifica=false;
+        this.visVisualizza=false;
+        
+        break;
+        case "modifica":
+          this.visInserisci=false;
+        this.visModifica=true;
+        this.visVisualizza=false;
+        break;
+        case "visualizza":
+          this.visInserisci=false;
+        this.visModifica=false;
+        this.visVisualizza=true;
+        break;
+    }
+  }
+
 }
