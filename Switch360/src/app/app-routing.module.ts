ee9a4f4c98c07d8c47a0adfa19e07cf99acdd4f0
JoJourdan <d.jourdan.2256@vallauri.edu>
2024-03-06 12:15:34 +0100

    Aggiunto routing module

--- Switch360/src/app/app-routing.module.ts
+++ Switch360/src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InserisciComponent } from './main/inserisci/inserisci.component';
+import { ModificaComponent } from './main/modifica/modifica.component';
+import { VisualizzaComponent } from './main/visualizza/visualizza.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  redirectTo: '/inserisci',
+  pathMatch: 'full'
+},
+{
+  path: 'inserisci', // senza slash !
+  component: InserisciComponent,
+  children: [
+    { path: 'box', component: InserisciComponent },
+
+    ]
+},{
+  path: 'modifica', // senza slash !
+  component: ModificaComponent,
+  children: [
+    { path: 'box', component: ModificaComponent },
+
+    ]
+},{
+  path: 'visualizza', // senza slash !
+  component: VisualizzaComponent,
+  children: [
+    { path: 'box', component: VisualizzaComponent },
+
+    ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Switch360/src/app/buttons/buttons.component.ts
+++ Switch360/src/app/buttons/buttons.component.ts
@@ -6,18 +6,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrl: './buttons.component.css'
 })
 export class ButtonsComponent {
-  @Output() featureSelected = new EventEmitter<string>()
 
-  selected:string="inserisci"
-
-  selectedClass:string="btn btn-primary ciccio"
-  defaultClass:string="btn btn-outline-primary ciccio"
-
-  onSelect(feature: string) {
-    console.log(this.selected)
-    this.selected=feature
-    this.featureSelected.emit(this.selected);
-  }
   
   
 }
