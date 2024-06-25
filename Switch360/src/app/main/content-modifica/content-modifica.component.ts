68c37762c97474393e6a2f1065710830c6b1b9ed
JoJourdan <d.jourdan.2256@vallauri.edu>
2024-06-25 16:22:24 +0200

    Summary
    Description

--- Switch360/src/app/main/content-modifica/content-modifica.component.ts
+++ Switch360/src/app/main/content-modifica/content-modifica.component.ts
@@ -29,6 +29,17 @@ export class ContentModificaComponent {
   portBackup?:porteSwitchModel
   switchBackup?:SwitchModel
 
+  nPort: number = this.switch.ports.length;
+  ports: PortModel[] = this.switch.ports;
+  selectedPort: PortModel = this.switch.ports[0]
+
+  portChange(n: number) {
+    this.nPort = n;
+    this.generaPorte();
+  }
+  selectPort(i: any) {
+    this.selectedPort = this.ports[i];
+  }
 
   ngOnInit() {
     console.log(this.route.params)
@@ -43,21 +54,46 @@ export class ContentModificaComponent {
     
   }
 
+  generaPorte() {
+    if (this.nPort != this.ports.length) {
+      if (this.nPort > this.ports.length) {
+        for (let i = this.ports.length; i < this.nPort; i++) {
+          let vlan = this.switchService.vlan[0];
+          let port = new PortModel(this.switchService.porte[0], vlan, '');
+          if (i < 24) port.number = '0/' + (i + 1);
+          else port.number = '1/' + (i - 23);
+          this.ports?.push(port);
+          if (i == 0) {
+            this.selectedPort = port;
+          }
+        }
+      } else {
+        for (let i = this.ports.length; i > this.nPort; i--) {
+          this.ports?.pop();
+        }
+      }
+    }
+  }
+
   eliminaBox(){
     this.switchService.box.splice(this.switchService.box.indexOf(this.box),1)
     this.box=this.switchService.box[0]
+    this.switchService.salvaDati()
   }
   eliminaVlan(){
     this.switchService.vlan.splice(this.switchService.vlan.indexOf(this.vlan),1)
     this.vlan=this.switchService.vlan[0]
+    this.switchService.salvaDati()
   }
   eliminaPorta(){
     this.switchService.porte.splice(this.switchService.porte.indexOf(this.port),1)
     this.port=this.switchService.porte[0]
+    this.switchService.salvaDati()
   }
   eliminaSwitch(){
     this.switchService.switch.splice(this.switchService.switch.indexOf(this.switch),1)
     this.switch=this.switchService.switch[0]
+    this.switchService.salvaDati()
   }
 
   
